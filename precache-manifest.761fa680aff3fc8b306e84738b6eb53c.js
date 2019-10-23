59a2d4cff7ff6d8234ef009adf303ef89f6bbc69
naor2205@gmail.com <naor2205@gmail.com>
2019-10-23 19:39:12 +0300

    Updates

---
+++ precache-manifest.761fa680aff3fc8b306e84738b6eb53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e8d9cd785444e770133006778e3107",
+    "url": "/sigalitot/index.html"
+  },
+  {
+    "revision": "7fb42681ac034bbfb5aa",
+    "url": "/sigalitot/static/css/main.d9d98666.chunk.css"
+  },
+  {
+    "revision": "3c268131005c6c134571",
+    "url": "/sigalitot/static/js/2.c20f7f17.chunk.js"
+  },
+  {
+    "revision": "7fb42681ac034bbfb5aa",
+    "url": "/sigalitot/static/js/main.28f4a1bc.chunk.js"
+  },
+  {
+    "revision": "0f1e6aac9ae7622c4a0c",
+    "url": "/sigalitot/static/js/runtime-main.6d02ee4a.js"
+  },
+  {
+    "revision": "440806ec90963e480e97753187b55258",
+    "url": "/sigalitot/static/media/migrash.440806ec.png"
+  }
+]);
